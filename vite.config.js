5c2ff7562026e467d87ae55420edcc2de19ef1bc
EriclesAlves13 <ericles45alves@gmail.com>
2026-07-16 21:51:38 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // IMPORTANTE:
 // - Se for publicar na Vercel ou Netlify, deixe base: "/" (é o padrão abaixo).
-// - Se for publicar no GitHub Pages, troque para "/NOME-DO-REPOSITORIO/".
+// - Se for publicar no GitHub Pages, troque para "/painel-padaria/".
 export default defineConfig({
   plugins: [react()],
   base: "/",
